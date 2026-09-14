983c431d6a6581d40fc495a2bc6a3553c1d246d0
Marvel <dajiowolabi6@gmail.com>
2026-09-14 02:05:33 -0700

    Day 15 prep: Static tickets.json data source, no longer depends on local json-server

--- src/hooks/useTickets.js
+++ src/hooks/useTickets.js
@@ -1,6 +1,6 @@
 import { useState, useEffect, useCallback } from 'react'
 
-const API_URL = 'http://localhost:3001/tickets'
+const API_URL = '/tickets.json'
 
 export function useTickets() {
   const [tickets, setTickets] = useState([])
